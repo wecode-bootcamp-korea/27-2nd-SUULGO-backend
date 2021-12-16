3fa6b59b687ac70a1daf841623e12bb4552e5c6b
Daco <dacokim32@gmail.com>
2021-12-16 13:37:06 +0900

    create : ProductView

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import KakaoLoginView, ProfileView
+from users.views import KakaoLoginView, ProfileView, ProductView
 
 urlpatterns = [
     path('/kakaologin', KakaoLoginView.as_view()),
-    path('/profile', ProfileView.as_view())
+    path('/profile', ProfileView.as_view()),
+    path('/<int:product_id>', ProductView.as_view()),
 ]
