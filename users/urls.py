60972927d9bfb188f6a6b8b6be47dbc7f98d7a9c
Daco <dacokim32@gmail.com>
2021-12-21 21:44:21 +0900

    상세페이지 리펙토링

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('/profile', ProfileView.as_view()),
     path('/<int:user_id>', UserView.as_view()),
     path('', UserListView.as_view()),
-    path('/promise', PromiseView.as_view()),
-    path('/promise-alarm', PromiseAlarmView.as_view())
+    path('/appointment', AppointmentView.as_view()),
+    path('/appointment-alarm', AppointmentAlarmView.as_view())
 ]
